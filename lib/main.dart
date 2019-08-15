ce9898afe6240bc1b66cc00048aac41d09438178
salaz310 <lrso.10@hotmail.com>
2019-08-15 12:44:07 -0600

    Se cambio el titulo de la aplicacion

--- lib/main.dart
+++ lib/main.dart
@@ -40,7 +40,7 @@ class ChatScreenState extends State<ChatScreen>{
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(title: new Text("Chat amigable")),
+      appBar: new AppBar(title: new Text("Chat para los amigos")),
       body: _buildTextComposer(),
     );
   }
